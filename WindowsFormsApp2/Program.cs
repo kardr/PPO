9c878d8f188d5d0f6bc8b3d765d9fab965969eb0
Карина Дроздова <K.Drozd33@yandex.ru>
2020-04-12 04:43:14 +0300

    test

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp2
         [STAThread]
         static void Main()
         {
+            //test
             Application.EnableVisualStyles();  
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GeneralForm());
